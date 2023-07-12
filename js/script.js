09c744896c9d678abbc48208209cb0f8928fa575
Tomasz Pietruczuk <tomekpietruczuk3@gmail.com>
2023-07-12 14:03:53 +0200

    changed names for calculateCurrency function

--- js/script.js
+++ js/script.js
@@ -14,11 +14,11 @@
       firstElement.value = temp;
    }
 
-   const calc = (event) => {
+   const calculateCurrency = (event) => {
       event.preventDefault()
       const first = firstElement.options[firstElement.selectedIndex].value;
       const second = secoundElemenet.options[secoundElemenet.selectedIndex].value;
-      const writedElement = document.querySelector(".js-valueCurrency").value;
+      const writedAmount = document.querySelector(".js-valueCurrency").value;
       const documentResult = document.querySelector(".js-documentResult");
 
       const eurUsd = 1.9 //usdEur (1/1.9)
@@ -27,31 +27,31 @@
 
       switch (true) {
          case first ==="EUR" &&second === "USD":
-            result = (eurUsd) * (writedElement)
+            result = (eurUsd) * (writedAmount)
             break;
 
          case first === "USD" && second === "EUR":
-            result = (1 / eurUsd) * (writedElement)
+            result = (1 / eurUsd) * (writedAmount)
             break;
 
          case first === "EUR" && second === "PLN":
-            result = (eurPln) * (writedElement)
+            result = (eurPln) * (writedAmount)
             break;
 
          case first === "PLN" && second === "EUR":
-            result = (1 / eurPln) * (writedElement)
+            result = (1 / eurPln) * (writedAmount)
             break;
 
          case first === "USD" && second === "PLN":
-            result = (usdPln) * (writedElement)
+            result = (usdPln) * (writedAmount)
             break;
 
          case first === "PLN" && second === "USD":
-            result = (1 / usdPln) * (writedElement)
+            result = (1 / usdPln) * (writedAmount)
             break;
 
          default:
-            result = (1) * (writedElement)
+            result = (1) * (writedAmount)
 
       }
       documentResult.value = result.toFixed(2);
@@ -60,21 +60,5 @@
    
    greetings();
    swapButton.addEventListener("click", swapper);
-   formElement.addEventListener("submit", calc);
-
-
-   /*Skrypt probny (nieudany) pod update z API => GET https://open.er-api.com/v6/latest/USD (request na select first) NIE UMIEM wydzielac DANYCH z api COS PROBOWALEM ALE chyba za daleko wybiegam
-   testbutton.addEventListener("click", () => {
-      fetch("https://open.er-api.com/v6/latest/USD") // /latest/${waluta wybrana wpierw}
-         .then(Response => {
-            return Response.json();
-         })
-         .then(data => {
-           console.log(data);
-           const currency = data.rates.55;
-           const currency_two = currency.PLN;
-         })
-   });
-   */
-
+   formElement.addEventListener("submit", calculateCurrency);
 }
